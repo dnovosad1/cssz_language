da2444e3b49860fc27b6199754f6802c32423154
gandalfthegray <stranavadavid@protonmail.com>
2023-12-07 00:06:25 +0100

    writovani do jasonu

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::fs::File;
-use std::io::Read;
-use csv::Writer;
-use serde_json::{Result, Value};
+use std::io::{Read, Write};
+use csv::{Reader, Writer};
+use serde_json::{json, Map, Result, Value};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Mutation {
+    key: String,
+    cs: String
+}
 
 fn get_input() -> String{
     let mut file = File::open("/home/gandalfthegray/language/src/input.json").expect("Failed to open file");
@@ -34,6 +41,7 @@ fn get_object_keys(data: &Value, prepend: String) -> Vec<(String, String)> {
 
 fn write_result(data: Vec<(String, String)>) -> Result<()> {
     let mut wtr = Writer::from_path("/home/gandalfthegray/language/src/result.csv").unwrap();
+    wtr.write_record(&["key", "cs"]).unwrap();
 
     for line in data.into_iter() {
         wtr.write_record(&[line.0, line.1]).unwrap();
@@ -43,9 +51,61 @@ fn write_result(data: Vec<(String, String)>) -> Result<()> {
     Ok(())
 }
 
-fn main(){
-    let v: Value = serde_json::from_str(&get_input()).unwrap();
+fn read_csv() -> Vec<Mutation> {
+    let file = File::open("/home/gandalfthegray/language/src/result.csv").expect("Failed to open CSV file");
 
-    let res =  get_object_keys(&v, String::from(""));
-    let _ = write_result(res);
+    let mut csv_reader = Reader::from_reader(file);
+    let mut mutations: Vec<Mutation> = Vec::new();
+
+    for result in csv_reader.deserialize::<Mutation>() {
+        match result {
+            Ok(record) => {
+                mutations.push(record);
+            }
+            Err(err) => {
+                eprintln!("Error reading CSV record: {}", err);
+            }
+        }
+    }
+
+    mutations
+}
+
+fn generate_json(data: Vec<Mutation>) {
+    let mut json_object = Map::new();
+
+    for mutation in data {
+        let mut current_map = &mut json_object;
+
+        let keys: Vec<&str> = mutation.key.split('.').collect();
+        let last_key = keys.last().expect("prazdny path kur*a");
+
+        for key in keys.iter().take(keys.len() - 1) {
+            current_map = current_map
+                .entry(key.to_string())
+                .or_insert_with(|| Value::Object(Map::new()))
+                .as_object_mut()
+                .expect("neco se do*ebalo a nemuze to najit object");
+        }
+
+        current_map.insert(last_key.to_string(), Value::String(mutation.cs));
+    }
+
+    let mut file = File::create("/home/gandalfthegray/language/src/output.json").expect("Failed to create file");
+
+    // Serialize the JSON object to a JSON string
+    let json_string = serde_json::to_string_pretty(&Value::Object(json_object))
+        .expect("serializace neprobehla");
+
+    // Write the JSON string to the file
+    file.write_all(json_string.as_bytes())
+        .expect("jak expectuju tak expectuju nepada to");
+}
+
+fn main(){
+    generate_json(read_csv());
+    // let v: Value = serde_json::from_str(&get_input()).unwrap();
+    //
+    // let res =  get_object_keys(&v, String::from(""));
+    // let _ = write_result(res);
 }
